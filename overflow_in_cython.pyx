4b5b6c08542fdddc68900b14e399637dfd2f85c0
Mosky <mosky.tw@gmail.com>
2014-04-15 21:44:25 +0800

    test overflow in cython

---
+++ overflow_in_cython.pyx
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from cython import sizeof
+
+def main():
+
+    assert sizeof(int) == 4
+    print 'sizeof(int) ->', sizeof(int)
+    # -2147483648 ~ 2147483647 if 32-bit int
+
+    cdef int i = 2147483647
+    assert(i == 2147483647)
+    print 'i ->', i
+
+    i += 1
+    assert(i == -2147483648);
+    print 'i ->', i
---
+++ test_overflow_in_cython.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import overflow_in_cython
+
+if __name__ == '__main__':
+    overflow_in_cython.main()
