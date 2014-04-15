fac1f56659192bae7047b1c10fdecaf061cb7ce0
Mosky <mosky.tw@gmail.com>
2014-04-15 21:20:09 +0800

    test_overflow_in_c.c

---
+++ test_overflow_in_c.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <assert.h>
+
+int main(int argc, const char *argv[]) {
+
+    assert(sizeof(int)== 4);
+    printf("sizeof int -> %lu\n", sizeof(int));
+    // -2147483648 ~ 2147483647 if 32-bit int
+
+    int i = 2147483647;
+    assert(i == 2147483647);
+    printf("i -> %d\n", i);
+
+    i += 1;
+    assert(i == -2147483648);
+    printf("i -> %d\n", i);
+
+    return 0;
+}
