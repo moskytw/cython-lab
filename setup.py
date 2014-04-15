538133615b94640950827f31566cc28bacc1b9bd
Mosky <mosky.tw@gmail.com>
2014-04-15 20:48:00 +0800

    setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from distutils.core import setup
+from Cython.Build import cythonize
+
+setup(
+    name = 'cython-lab',
+    ext_modules = cythonize("hello.pyx"),
+)
