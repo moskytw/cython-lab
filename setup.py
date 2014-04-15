8a26817606b1da42f81aaaa044b60df7738cdfcf
Mosky <mosky.tw@gmail.com>
2014-04-15 21:36:52 +0800

    find the pyx files automatically

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from glob import glob
 from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(
     name = 'cython-lab',
-    ext_modules = cythonize("hello.pyx"),
+    # cythonize accepts a list
+    # pydoc Cython.Build.cythonize
+    ext_modules = cythonize(glob('*.pyx')),
 )
