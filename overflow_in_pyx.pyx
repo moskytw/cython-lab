1e1254b8b278b530caea72a1d1234a8f9593895f
Mosky <mosky.tw@gmail.com>
2019-12-10 08:52:23 +0800

    Fix the asserts syntaxes

--- overflow_in_pyx.pyx
+++ overflow_in_pyx.pyx
@@ -8,9 +8,9 @@ def main():
     # -2147483648 ~ 2147483647 if 32-bit int
 
     cdef int i = 2147483647
-    assert(i == 2147483647)
+    assert i == 2147483647
     print('i ->', i)
 
     i += 1
-    assert(i == -2147483648)
+    assert i == -2147483648
     print('i ->', i)
