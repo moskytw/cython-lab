7b5c715be292c957d61922ed72629ae2f83ec737
Mosky <mosky.tw@gmail.com>
2019-12-11 03:18:50 +0800

    Fix the prints

--- use_lib_in_pyx.pyx
+++ use_lib_in_pyx.pyx
@@ -1,13 +1,13 @@
 import lib_in_pyx
 cimport lib_in_pyx  # needs lib_in_pyx.pxd
 
-print lib_in_pyx.say_hello()
-print lib_in_pyx.say_hello(b'Mosky')
-print
+print(lib_in_pyx.say_hello())
+print(lib_in_pyx.say_hello(b'Mosky'))
+print()
 
-print lib_in_pyx.say_hello_in_c()
-print lib_in_pyx.say_hello_in_c(b'Mosky')
-print
+print(lib_in_pyx.say_hello_in_c())
+print(lib_in_pyx.say_hello_in_c(b'Mosky'))
+print()
 
-print lib_in_pyx.say_hello_by_cpdef()
-print lib_in_pyx.say_hello_by_cpdef(b'Mosky')
+print(lib_in_pyx.say_hello_by_cpdef())
+print(lib_in_pyx.say_hello_by_cpdef(b'Mosky'))
