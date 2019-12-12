c38136e554849722c40e022bd349fd352184519b
Mosky <mosky.tw@gmail.com>
2019-12-12 09:25:47 +0800

    Refine point.pyx

--- point.pyx
+++ point.pyx
@@ -4,6 +4,17 @@ cdef struct Point:
 
 
 def main():
+
     d = {'x': 10, 'y': 20}
+
+    # test Python dict to C struct
+
     cdef Point p = d
-    print(p.get('z'))
+    assert p.x == 10
+    # assert p['y'] == 20
+    # # -> Attempting to index non-array type 'Point'
+
+    # test C struct to Python dict
+
+    assert dict(p)['y'] == 20
+    assert p.get('z') is None
